a98288d643d30cd2c7aa17a4512c9102eb8b95b0
Vaibhavi Karn <76275888+vaibhavikarn2001@users.noreply.github.com>
2021-11-25 23:19:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const container=document.querySelector('.container');
+const sizeEl=document.querySelector('.size');
+let size=sizeEl.value;
+const color=document.querySelector('.color');
+const resetBTN=document.querySelector('.btn')
+
+let draw= false;
+
+function populate(size){
+    container.style.setProperty('--size',size)
+for(let i=0; i< size * size; i++){
+    const div= document.createElement('div');
+    div.classList.add('pixel');
+
+div.addEventListener('mouseover', function(){
+    if(!draw) return
+    div.style.backgroundColor= color.value;
+})
+
+div.addEventListener('mousedown', function(){
+    div.style.backgroundColor= color.value;
+})
+    container.appendChild(div);
+}
+
+}
+window.addEventListener('mousedown',function(){
+    draw=true;
+})
+window.addEventListener('mouseup',function(){
+    draw=false;
+})
+function reset(){
+    container.innerHTML=''
+    populate(size)
+}
+resetBTN.addEventListener('click',reset)
+
+sizeEl.addEventListener('change', function(){
+    size=sizeEl.value
+    reset()
+})
+populate(size)
